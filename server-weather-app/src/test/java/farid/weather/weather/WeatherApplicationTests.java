8cca91141afa5bb36f5b7dff7f8e6a3fcc4ceb68
Farid Lopez <faridlopez5@gmail.com>
2024-08-12 08:20:06 -0700

    setting up server side repo first, working on checklist now

---
+++ server-weather-app/src/test/java/farid/weather/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package farid.weather.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
