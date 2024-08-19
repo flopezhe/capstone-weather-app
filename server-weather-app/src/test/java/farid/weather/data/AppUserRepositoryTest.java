c8f8c45d3db7b818b932c26ffb005f18fe6cc639
Farid Lopez <faridlopez5@gmail.com>
2024-08-18 19:40:55 -0700

    testing set up making another branch now

---
+++ server-weather-app/src/test/java/farid/weather/data/AppUserRepositoryTest.java
@@ -0,0 +1,17 @@
+package farid.weather.data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppUserRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByUserName() {
+    }
+}
---
+++ server-weather-app/src/test/java/farid/weather/domain/AppUserServiceTest.java
@@ -0,0 +1,29 @@
+package farid.weather.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppUserServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createAppUser() {
+    }
+
+    @Test
+    void getAppUser() {
+    }
+
+    @Test
+    void deleteAppUser() {
+    }
+
+    @Test
+    void updateAppUser() {
+    }
+}
