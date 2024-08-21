205d886697dd3eea7a5eaf577bc9e7ea5fffaa3c
Farid Lopez <faridlopez5@gmail.com>
2024-08-21 14:40:31 -0700

    starting domain layer testing, currently in appuser

--- server-weather-app/src/test/java/farid/weather/domain/AppUserServiceTest.java
+++ server-weather-app/src/test/java/farid/weather/domain/AppUserServiceTest.java
@@ -1,19 +1,27 @@
 package farid.weather.domain;
 
-import org.junit.jupiter.api.BeforeEach;
+import farid.weather.data.AppUserRepository;
+import farid.weather.models.AppUser;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 
-@MockBean
+@SpringBootTest
 class AppUserServiceTest {
 
-    
+    @MockBean
+    AppUserRepository appUserRepository;
 
+    @Autowired
+    AppUserService appUserService;
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void getAppUsers() {
+        AppUser appUser1 = new AppUser("1", "user1", "password1","random1");
     }
+
 
     @Test
     void createAppUser() {
