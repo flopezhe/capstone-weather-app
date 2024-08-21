e088431de8952996467e1576f4f97790a6e34087
Farid Lopez <faridlopez5@gmail.com>
2024-08-21 16:16:55 -0700

    mvp domain testing complete, might add more tomorrow if time allows after UI enhancement last touches

--- server-weather-app/src/test/java/farid/weather/domain/ClothingItemServiceTest.java
+++ server-weather-app/src/test/java/farid/weather/domain/ClothingItemServiceTest.java
@@ -1,37 +1,82 @@
 package farid.weather.domain;
 
-import org.junit.jupiter.api.BeforeEach;
+import farid.weather.data.ClothingItemRepository;
+import farid.weather.models.ClothingItem;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
 class ClothingItemServiceTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+   @MockBean
+   ClothingItemRepository clothingItemRepository;
 
-    @Test
-    void findClothingItemById() {
-    }
+   @Autowired
+    ClothingItemService clothingItemService;
 
-    @Test
-    void getClothingItemsByType() {
-    }
-
-    @Test
-    void getClothingItemsForUser() {
-    }
 
     @Test
     void addClothingItem() {
+        ClothingItem clothingItem = new ClothingItem("1", "bottom", "blue shirt", "fakeurl1", "yes", "no", "user1");
+
+        when(clothingItemRepository.save(any())).thenReturn(clothingItem);
+
+
+        clothingItemService.addClothingItem(clothingItem);
+        List<ClothingItem> actual = clothingItemRepository.findByClothingTypeAndUserId("bottom","user1");
+
+        assertNotNull(actual);
+    }
+
+    @Test
+    void shouldNotAddClothingItemWithoutType(){
+        ClothingItem clothingItem = new ClothingItem("1", "", "blue shirt", "fakeurl1", "yes", "no", "user1");
+
+        clothingItemService.addClothingItem(clothingItem);
+
+        Result<ClothingItem> expected = clothingItemService.updateClothingItem(clothingItem);
+
+        assertFalse(expected.isSuccess());
     }
 
     @Test
     void deleteClothingItem() {
+        ClothingItem clothingItem = new ClothingItem("10", "bottom", "blue shirt", "fakeurl1", "yes", "no", "user1");
+
+        when(clothingItemRepository.existsById("10")).thenReturn(false);
+
+        clothingItemService.deleteClothingItem("10");
+
+        boolean actual = clothingItemRepository.existsById("10");
+
+        assertFalse(actual);
+
+
+
     }
 
     @Test
-    void updateClothingItem() {
+    void updateClothingItem(){
+        ClothingItem clothingItem = new ClothingItem("1", "bottom", "blue shirt", "fakeurl1", "yes", "no", "user1");
+
+
+        when(clothingItemRepository.save(any())).thenReturn(clothingItem);
+
+        Result<ClothingItem> expected = new Result<>();
+        expected.setPayload(clothingItem);
+        Result<ClothingItem> actual = clothingItemService.updateClothingItem(clothingItem);
+        actual.setPayload(clothingItem);
+
+        assertNotNull(actual);
     }
+
 }
--- server-weather-app/src/test/java/farid/weather/domain/WeatherServiceTest.java
+++ server-weather-app/src/test/java/farid/weather/domain/WeatherServiceTest.java
@@ -1,33 +1,80 @@
 package farid.weather.domain;
 
-import org.junit.jupiter.api.BeforeEach;
+import farid.weather.data.WeatherRepository;
+import farid.weather.models.Weather;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+@SpringBootTest
 class WeatherServiceTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @MockBean
+    WeatherRepository weatherRepository;
 
-    @Test
-    void getWeather() {
-    }
+    @Autowired
+    WeatherService weatherService;
+
+
 
     @Test
     void saveWeather() {
+        Weather weather = new Weather("1", 1, 1, "user1");
+
+        when(weatherRepository.save(any())).thenReturn(weather);
+
+        Result <Weather> actual = weatherService.saveWeather(weather);
+
+        assertTrue(actual.isSuccess());
+
+    }
+
+    @Test
+    void shouldNotAddInvalidLatitude(){
+        Weather weather = new Weather("1", 91, 1, "user1");
+
+        Result <Weather> actual = weatherService.saveWeather(weather);
+
+        assertFalse(actual.isSuccess());
+    }
+
+    @Test
+    void shouldNotAddInvalidLongitude(){
+        Weather weather = new Weather("1", 1, 181, "user1");
+
+        Result <Weather> actual = weatherService.saveWeather(weather);
+
+        assertFalse(actual.isSuccess());
+
     }
 
     @Test
     void deleteWeather() {
+        Weather weather = new Weather("1", 1, 1, "user1");
+
+        when(weatherRepository.existsById("1")).thenReturn(false);
+
+        boolean actual = weatherService.deleteWeather("1");
+
+        assertFalse(actual);
     }
 
     @Test
     void updateWeather() {
+        Weather weather = new Weather("1", 1, 1, "user1");
+
+        when(weatherRepository.save(any())).thenReturn(weather);
+
+        Result <Weather> actual = weatherService.updateWeather(weather);
+
+        assertTrue(actual.isSuccess());
     }
 
-    @Test
-    void validateSave() {
-    }
+   
 }
